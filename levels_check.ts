0f5c0a50a0ee5e0522047b7ad6d8e59533c3b9d1
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2023-01-17 13:07:52 +0100

    Update main.ts, levels_check.ts

--- levels_check.ts
+++ levels_check.ts
@@ -16,26 +16,23 @@ enum Seeds {
     ChorusPlant,
 }
 
-let correctPositions: Position[] = [];
-
-
 function levelCheck(seed: Seeds) {
     agent.setSlot(seed + 1)
     agent.place(DOWN)
     const detectPostion = world(agent.getPosition().getValue(Axis.X), agent.getPosition().getValue(Axis.Y) - 1, agent.getPosition().getValue(Axis.Z))
     const detectPostionChorus = world(agent.getPosition().getValue(Axis.X), agent.getPosition().getValue(Axis.Y) - 2, agent.getPosition().getValue(Axis.Z))
 
-    if (correctPositions.indexOf(detectPostion) === -1) {
+    if (correctPositions.indexOf(detectPostion.toString()) === -1) {
         if (blocks.testForBlock(FARMLAND, detectPostion) && seed === Seeds.Seed) {
-            correctPositions.push(detectPostion)
+            correctPositions.push(detectPostion.toString())
         } else if (blocks.testForBlock(DIRT, detectPostion) && seed === Seeds.Flower) {
-            correctPositions.push(detectPostion)
+            correctPositions.push(detectPostion.toString())
         } else if (blocks.testForBlock(SAND, detectPostionChorus) && seed === Seeds.Pumpkin) {
-            correctPositions.push(detectPostion)
+            correctPositions.push(detectPostion.toString())
         } else if (blocks.testForBlock(ENDSTONE, detectPostionChorus) && seed === Seeds.ChorusPlant) {
-            correctPositions.push(detectPostionChorus)
+            correctPositions.push(detectPostionChorus.toString())
         } else if (blocks.testForBlock(CHORUS_PLANT, detectPostionChorus) && seed === Seeds.ChorusFlower) {
-            correctPositions.push(detectPostionChorus)
+            correctPositions.push(detectPostionChorus.toString())
         } else {
             player.execute(`execute @c ~ ~ ~ particle rwm:barrier ~ ~1 ~`)
             blocks.place(RED_CONCRETE, detectPostion);
